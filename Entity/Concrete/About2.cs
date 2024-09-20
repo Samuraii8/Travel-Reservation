8f788c8f2e55d03e98b280443851cd9164ac7fef
Oxygen808 <mehmetokur756@gmail.com>
2024-09-20 09:41:39 +0300

    About, Contact, Destination, Feature, Guide, Newsletter, SubAbout and Reference class Objects added to Entitiy layer

---
+++ Entity/Concrete/About2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Concrete
+{
+    public class About2
+    { 
+        public int About2ID { get; set; }
+        public string? Title1 { get; set; }
+        public string? Title2 { get; set; }
+        public string? Descrpition { get; set; }
+        public string? Image { get; set; }
+    }
+}
