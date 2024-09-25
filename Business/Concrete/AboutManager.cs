fcfb45c6a2015b67020f3b515a116c1e0f939e02
Oxygen808 <mehmetokur756@gmail.com>
2024-09-25 15:00:56 +0300

    added FluentValidation support to the business layer

---
+++ Business/Concrete/AboutManager.cs
@@ -0,0 +1,46 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class AboutManager : IAboutService
+    {
+        // dependency injection 
+        IAboutDal _aboutDal;
+
+        public AboutManager(IAboutDal aboutDal)
+        {
+                _aboutDal = aboutDal;
+        }
+        public void TAdd(About t)
+        {
+            _aboutDal.Instert(t);
+        }
+
+        public void TDelete(About t)
+        {
+          _aboutDal.Delete(t);
+        }
+
+        public About TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<About> TGetList()
+        {
+            return _aboutDal.GetList();
+        }
+
+        public void TUpdate(About t)
+        {
+            _aboutDal.Update(t);
+        }
+    }
+}
